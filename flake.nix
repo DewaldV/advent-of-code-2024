3c7a4671856d1a4f7ba206cc1b1e30f9ff88847f
DewaldV <dewald.viljoen@pm.me>
2024-11-22 21:51:14 +0000

    chore: initial setup for nix and direnv

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.follows = "rust-overlay/nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          nativeBuildInputs = [
+            pkg-config
+            rust-bin.stable.latest.complete
+            clang
+            mold
+
+          ];
+          buildInputs = [ openssl ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
+}
