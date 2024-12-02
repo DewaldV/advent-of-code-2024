510fad5946eff08083ffb0e9628ef86ab0c3d583
DewaldV <dewald.viljoen@pm.me>
2024-12-02 10:16:46 +0000

    day03: scaffold day

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day_01;
 mod day_02;
+mod day_03;
 mod util;
 
 pub use self::util::read_to_string;
@@ -7,4 +8,5 @@ pub use self::util::read_to_string;
 fn main() {
     day_01::run();
     day_02::run();
+    day_03::run();
 }
