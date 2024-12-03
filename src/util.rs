4bd966c5276461d28bbc585909791498fa6966e3
DewaldV <dewald.viljoen@pm.me>
2024-12-03 09:00:02 +0000

    refactor: print path on file read error

--- src/util.rs
+++ src/util.rs
@@ -2,8 +2,11 @@ use std::{fs, path::Path};
 
 pub fn read_to_string(file: &str) -> String {
     let path = Path::new(&file);
-    let content = fs::read_to_string(path).expect("expected file to exist");
-    return content;
+    let content = fs::read_to_string(path);
+    match content {
+        Ok(c) => return c,
+        Err(e) => panic!("unable to read file: {}, err: {}", file, e),
+    }
 }
 
 pub fn run_day(day: u8, puzzle_01: &dyn Fn(&str) -> i32, puzzle_02: &dyn Fn(&str) -> i32) {
