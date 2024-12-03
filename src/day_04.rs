0b2be9a3370689cde9ce8d525fc714a50d0e4d56
DewaldV <dewald.viljoen@pm.me>
2024-12-03 09:00:44 +0000

    day04: scaffold

---
+++ src/day_04.rs
@@ -0,0 +1,8 @@
+use crate::util::run_day;
+
+pub mod puzzle_01;
+pub mod puzzle_02;
+
+pub fn run() {
+    run_day(4, &puzzle_01::solve, &puzzle_02::solve)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 mod util;
 
 pub use self::util::read_to_string;
@@ -9,4 +10,5 @@ fn main() {
     day_01::run();
     day_02::run();
     day_03::run();
+    day_04::run();
 }
