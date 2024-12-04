9fe195b6e8e3878214c69e287647e8264691e849
DewaldV <dewald.viljoen@pm.me>
2024-12-04 08:09:50 +0000

    day04 completed

--- src/day_04/puzzle_01.rs
+++ src/day_04/puzzle_01.rs
@@ -1,8 +1,55 @@
+use std::collections::HashMap;
+
 use crate::read_to_string;
 
 pub fn solve(file: &str) -> i32 {
-    let _content = read_to_string(file);
-    return 0;
+    let content = read_to_string(file);
+
+    let letters: Vec<Vec<char>> = content.lines().map(|l| l.chars().collect()).collect();
+
+    let mut vertical: Vec<String> = Vec::new();
+    let mut left_diagonal = HashMap::new();
+    let mut right_diagonal = HashMap::new();
+
+    for (r, line) in letters.iter().enumerate() {
+        for (c, ch) in line.iter().enumerate() {
+            if r == 0 {
+                vertical.insert(c, String::new())
+            }
+
+            let l = vertical.get_mut(c).unwrap();
+            l.push(ch.clone());
+
+            let left_key = i32::try_from(r).unwrap() - i32::try_from(c).unwrap();
+            if !left_diagonal.contains_key(&left_key) {
+                left_diagonal.insert(left_key, String::new());
+            }
+
+            let l = left_diagonal.get_mut(&left_key).unwrap();
+            l.push(ch.clone());
+
+            let right_key = r + c;
+            if !right_diagonal.contains_key(&right_key) {
+                right_diagonal.insert(right_key, String::new());
+            }
+
+            let l = right_diagonal.get_mut(&right_key).unwrap();
+            l.push(ch.clone());
+        }
+    }
+
+    let horizontal_count: usize = content.lines().map(|l| count_matches(&l)).sum();
+    let vertical_count: usize = vertical.iter().map(|l| count_matches(&l)).sum();
+    let left_diagonal_count: usize = left_diagonal.iter().map(|(_, l)| count_matches(&l)).sum();
+    let right_diagonal_count: usize = right_diagonal.iter().map(|(_, l)| count_matches(&l)).sum();
+
+    let total = horizontal_count + vertical_count + left_diagonal_count + right_diagonal_count;
+
+    return total.try_into().unwrap();
+}
+
+fn count_matches(line: &str) -> usize {
+    line.matches("XMAS").count() + line.matches("SAMX").count()
 }
 
 #[cfg(test)]
@@ -10,16 +57,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_one_example() {
-        // let report: Vec<i32> = vec![7, 6, 4, 2, 1];
-        // let safe = report_safe(&report);
-        // assert_eq!(safe, true)
-    }
-
-    #[test]
     fn test_example_file() {
         let example_input_file = "src/day_04/example_input_01";
-        let expected_answer = 0;
+        let expected_answer = 18;
 
         let answer = solve(example_input_file);
 
--- src/day_04/puzzle_02.rs
+++ src/day_04/puzzle_02.rs
@@ -1,8 +1,39 @@
 use crate::read_to_string;
 
 pub fn solve(file: &str) -> i32 {
-    let _content = read_to_string(file);
-    return 0;
+    let content = read_to_string(file);
+
+    let mut count = 0;
+
+    let letters: Vec<Vec<char>> = content.lines().map(|l| l.chars().collect()).collect();
+    for (r, line) in letters.iter().enumerate() {
+        for (c, ch) in line.iter().enumerate() {
+            if r == 0 || c == 0 || r == letters.len() - 1 || c == line.len() - 1 {
+                continue;
+            }
+
+            if *ch == 'A' {
+                let top_left = letters[r - 1][c - 1];
+                let top_right = letters[r - 1][c + 1];
+                let bottom_left = letters[r + 1][c - 1];
+                let bottom_right = letters[r + 1][c + 1];
+
+                let mut left = String::new();
+                left.push(top_left);
+                left.push(bottom_right);
+
+                let mut right = String::new();
+                right.push(top_right);
+                right.push(bottom_left);
+
+                if (left == "MS" || left == "SM") && (right == "MS" || right == "SM") {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    return count.try_into().unwrap();
 }
 
 #[cfg(test)]
@@ -10,16 +41,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_one_example() {
-        // let report: Vec<i32> = vec![7, 6, 4, 2, 1];
-        // let safe = report_safe(&report);
-        // assert_eq!(safe, true)
-    }
-
-    #[test]
     fn test_example_file() {
         let example_input_file = "src/day_04/example_input_01";
-        let expected_answer = 0;
+        let expected_answer = 9;
 
         let answer = solve(example_input_file);
 
