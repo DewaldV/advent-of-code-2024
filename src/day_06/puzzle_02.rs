d803e391724e465ed1be7698ad48c0d8e252189e
DewaldV <dewald.viljoen@pm.me>
2024-12-05 10:06:08 +0000

    day06: scaffold

---
+++ src/day_06/puzzle_02.rs
@@ -0,0 +1,20 @@
+pub fn solve(_content: &str) -> i32 {
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::util::run_example_file;
+
+    use super::*;
+
+    #[test]
+    fn test_one_example() {
+        assert_eq!(true, true);
+    }
+
+    #[test]
+    fn test_example_file() {
+        run_example_file(6, 0, &solve);
+    }
+}
