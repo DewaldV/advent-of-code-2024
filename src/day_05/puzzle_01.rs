ad44332e04a0324a6df3b18f988d3f5e0f4f1f26
DewaldV <dewald.viljoen@pm.me>
2024-12-05 10:03:26 +0000

    day05 completed

--- src/day_05/puzzle_01.rs
+++ src/day_05/puzzle_01.rs
@@ -1,5 +1,44 @@
-pub fn solve(_content: &str) -> i32 {
-    return 0;
+use std::collections::HashMap;
+
+pub fn solve(content: &str) -> i32 {
+    let split_content = content
+        .split_once("\n\n")
+        .expect("one instance of double new line");
+
+    let mut rules = HashMap::new();
+    split_content.0.lines().for_each(|l| {
+        let rule = l.split_once('|').expect("pipe seperated");
+        if !rules.contains_key(rule.0) {
+            rules.insert(rule.0, Vec::new());
+        }
+
+        rules.get_mut(rule.0).unwrap().push(rule.1);
+    });
+
+    let updates: Vec<Vec<&str>> = split_content
+        .1
+        .lines()
+        .map(|l| l.split(',').collect())
+        .collect();
+
+    let middle_sum = updates
+        .iter()
+        .filter(|update| {
+            update.is_sorted_by(|a, b| {
+                if let Some(r) = rules.get(b) {
+                    return !r.contains(&a);
+                }
+                true
+            })
+        })
+        .map(|update| {
+            let mid = update.len() / 2;
+            let mid_val = update.get(mid).unwrap();
+            (*mid_val).parse::<i32>().unwrap()
+        })
+        .sum();
+
+    return middle_sum;
 }
 
 #[cfg(test)]
@@ -15,6 +54,6 @@ mod tests {
 
     #[test]
     fn test_example_file() {
-        run_example_file(5, 0, &solve);
+        run_example_file(5, 143, &solve);
     }
 }
--- src/day_05/puzzle_02.rs
+++ src/day_05/puzzle_02.rs
@@ -1,5 +1,54 @@
-pub fn solve(_content: &str) -> i32 {
-    return 0;
+use std::{cmp::Ordering, collections::HashMap};
+
+pub fn solve(content: &str) -> i32 {
+    let split_content = content
+        .split_once("\n\n")
+        .expect("one instance of double new line");
+
+    let mut rules = HashMap::new();
+    split_content.0.lines().for_each(|l| {
+        let rule = l.split_once('|').expect("pipe seperated");
+        if !rules.contains_key(rule.0) {
+            rules.insert(rule.0, Vec::new());
+        }
+
+        rules.get_mut(rule.0).unwrap().push(rule.1);
+    });
+
+    let updates: Vec<Vec<&str>> = split_content
+        .1
+        .lines()
+        .map(|l| l.split(',').collect())
+        .collect();
+
+    let middle_sum = updates
+        .into_iter()
+        .filter(|update| {
+            !update.is_sorted_by(|a, b| {
+                if let Some(r) = rules.get(b) {
+                    return !r.contains(&a);
+                }
+                true
+            })
+        })
+        .map(|update| {
+            let mut l_update = update.clone();
+            l_update.sort_by(|a, b| {
+                if let Some(r) = rules.get(b) {
+                    if r.contains(&a) {
+                        return Ordering::Greater;
+                    }
+                }
+                Ordering::Less
+            });
+
+            let mid = l_update.len() / 2;
+            let mid_val = l_update.get(mid).unwrap();
+            (*mid_val).parse::<i32>().unwrap()
+        })
+        .sum();
+
+    return middle_sum;
 }
 
 #[cfg(test)]
@@ -15,6 +64,6 @@ mod tests {
 
     #[test]
     fn test_example_file() {
-        run_example_file(5, 0, &solve);
+        run_example_file(5, 123, &solve);
     }
 }
